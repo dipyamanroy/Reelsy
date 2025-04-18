307d7255bade0a5a7cb6b62f43cd17cd81327fdf
dipyamanroy <d1pyamanroy@protonmail.com>
2025-04-18 12:22:45 +0530

    Added form data handling

--- app/(main)/create/page.jsx
+++ app/(main)/create/page.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Topic from './_components/Topic'
 
 function Create() {
+    const [ formData, setFormData ] = useState();
+
+    const onHandleInputChange = (fieldName, fieldValue) => {
+        setFormData(prev =>({
+            ...prev,
+            [fieldName]:fieldValue
+        }))
+    }
+    
     return (
         <div>
             <h2 className='text-3xl'>Create</h2>
             <h2 className='text-md text-gray-400'>Start your creative journey here</h2>
-            {/* Topic and Script */}
-            <Topic />
-            {/* Art style */}
+            <div className='grid grid-cols-1 md:grid-cols-3 mt-8'>
+                <div className='col-span-2 p-7 border rounded-xl'>
+                    {/* Topic and Script */}
+                    <Topic />
+                    {/* Art style */}
 
-            {/* Voice */}
+                    {/* Voice */}
 
-            {/* Captions */}
+                    {/* Captions */}
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
