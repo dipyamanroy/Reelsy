221c7c0314a436d34d27b0e350a123cf162abfcf
dipyamanroy <d1pyamanroy@protonmail.com>
2025-04-18 15:30:35 +0530

    Added voice selection component

---
+++ app/(main)/create/_components/Voice.jsx
@@ -0,0 +1,75 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
+import React, { useState } from 'react'
+
+const voiceOptions = [
+    {
+        "value": "alloy",
+        "name": "🇺🇸 Alloy (Female)"
+    },
+    {
+        "value": "ash",
+        "name": "🇺🇸 Ash (Male)"
+    },
+    {
+        "value": "ballad",
+        "name": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Ballad (Male)"
+    },
+    {
+        "value": "coral",
+        "name": "🇺🇸 Coral (Female)"
+    },
+    {
+        "value": "echo",
+        "name": "🇺🇸 Echo (Male)"
+    },
+    {
+        "value": "fable",
+        "name": "🏴󠁧󠁢󠁥󠁮󠁧󠁿 Fable (Male)"
+    },
+    {
+        "value": "onyx",
+        "name": "🇺🇸 Onyx (Male)"
+    },
+    {
+        "value": "nova",
+        "name": "🇺🇸 Nova (Feale)"
+    },
+    {
+        "value": "sage",
+        "name": "🇺🇸 Sage (Female)"
+    },
+    {
+        "value": "shimmer",
+        "name": "🇺🇸 Shimmer (Female)"
+    },
+    {
+        "value": "verse",
+        "name": "🇺🇸 Verse (Male)"
+    },
+]
+
+function Voice({ onHandleInputChange }) {
+    const [selectedVoice, setSelectedVoice] = useState();
+
+    return (
+        <div className='mt-6'>
+            <h2>Voice</h2>
+            <p className='text-sm text-gray-400 mb-1'>Select voice style for your video</p>
+            <ScrollArea className='h-[150px] w-full'>
+            <div className='grid grid-cols-2 gap-3'>
+                {voiceOptions.map((voice, index) => (
+                    <h2 className={`cursor-pointer p-3 dark:bg-gray-900 dark:border-white rounded-lg hover:border ${voice.name == selectedVoice && 'border'}`}
+                    key={index}
+                    onClick={() => {setSelectedVoice(voice.name)
+                        onHandleInputChange('voice', voice.value)
+                    }}>
+                        {voice.name}
+                    </h2>
+                ))}
+            </div>
+            </ScrollArea>
+        </div>
+    )
+}
+
+export default Voice
--- app/(main)/create/page.jsx
+++ app/(main)/create/page.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Topic from './_components/Topic'
 import ArtStyle from './_components/ArtStyle';
 import Voice from './_components/Voice';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 function Create() {
     const [ formData, setFormData ] = useState();
@@ -20,7 +21,8 @@ function Create() {
             <h2 className='text-3xl'>Create</h2>
             <h2 className='text-md text-gray-400'>Start your creative journey here</h2>
             <div className='grid grid-cols-1 md:grid-cols-3 mt-8'>
-                <div className='col-span-2 p-7 border rounded-xl h-[70vh] overflow-auto'>
+                <div className='col-span-2 p-7 border rounded-xl h-[70vh]'>
+                <ScrollArea className='h-[500px] w-full'>
                     {/* Topic and Script */}
                     <Topic onHandleInputChange={onHandleInputChange} />
                     {/* Art style */}
@@ -29,9 +31,9 @@ function Create() {
                     <Voice onHandleInputChange={onHandleInputChange} />
 
                     {/* Captions */}
+                </ScrollArea>
                 </div>
                 <div>
-
                 </div>
             </div>
         </div>
