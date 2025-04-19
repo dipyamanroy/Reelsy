b2a65a6acd834d4a0f0876f1b54d8b797ffb671a
dipyamanroy <d1pyamanroy@protonmail.com>
2025-04-19 12:28:09 +0530

    Made UI tweaks

--- app/(main)/create/_components/Preview.jsx
+++ app/(main)/create/_components/Preview.jsx
@@ -18,7 +18,7 @@ function Preview({ formData }) {
                 />
             ) : (
                 <div className='w-full h-[65vh] dark:bg-primary-foreground flex items-center justify-center rounded-xl border border-dashed border-gray-300'>
-                    <p className='text-gray-500 text-lg'>Select a style to see preview</p>
+                    <p className="text-gray-500 text-xs text-center">Select a style to see preview</p>
                 </div>
             )}
 
