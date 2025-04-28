6967b98ac45c32b2c3a71da56b6c9ae1e4fbed05
dipyamanroy <d1pyamanroy@protonmail.com>
2025-04-28 08:55:26 +0530

    Design element consistency

--- app/(main)/create/page.jsx
+++ app/(main)/create/page.jsx
@@ -74,6 +74,10 @@ function Create() {
 
     return (
         <div>
+            <div
+            className="absolute w-60 h-60 bg-gradient-to-br from-green-400 via-blue-400 to-blue-500 rounded-full blur-2xl opacity-40 z-[-20] pointer-events-none animate-pulse shadow-[0_0_80px_30px_rgba(34,197,94,0.3)]"
+            style={{ top: '-100px', right: '25px' }}
+            />
             <h2 className='text-3xl'>Create</h2>
             <h2 className='text-md text-gray-400'>Start your creative journey here</h2>
             <div className='grid grid-cols-1 md:grid-cols-3 mt-8 gap-10'>
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,6 +4,10 @@ import VideoList from './_components/VideoList'
 function Dashboard() {
     return (
         <div>
+        <div
+        className="absolute w-60 h-60 bg-gradient-to-br from-green-400 via-blue-400 to-blue-500 rounded-full blur-2xl opacity-40 z-[-20] pointer-events-none animate-pulse shadow-[0_0_80px_30px_rgba(34,197,94,0.3)]"
+        style={{ top: '-100px', right: '25px' }}
+        />
             <h2 className='text-3xl'>Dashboard</h2>
             <h2 className='text-md text-gray-400'>My videos</h2>
             <VideoList />
