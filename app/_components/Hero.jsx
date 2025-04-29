e586504f6fc57ac682b540c9e4e586842c93cdcb
dipyamanroy <d1pyamanroy@protonmail.com>
2025-04-29 09:56:21 +0530

    UI change

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -7,8 +7,8 @@ import Authentication from './Authentication'
 function Hero() {
     return (
         <section className="relative z-10 px-4 py-16 sm:py-24 flex flex-col items-center text-center gap-6 sm:gap-8 md:px-20 lg:px-36 xl:px-48">
-            <h1 className="mt-24 font-extrabold text-4xl sm:text-5xl md:text-6xl leading-tight">
-                Welcome to <span className="text-gradient bg-gradient-to-br from-green-400 to-blue-500 bg-clip-text text-transparent">Reelsy</span>
+            <h1 className="mt-24 font-regular text-4xl sm:text-5xl md:text-6xl leading-tight">
+                Welcome to <span className="font-bold text-gradient bg-gradient-to-br from-green-400 to-blue-500 bg-clip-text text-transparent">Reelsy</span>
             </h1>
 
             <p className="text-lg sm:text-xl md:text-2xl text-gray-500 max-w-3xl">
