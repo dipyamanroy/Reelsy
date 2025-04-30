9183178d95872cd2c4c648d8838cfa6506477adb
dipyamanroy <d1pyamanroy@protonmail.com>
2025-04-30 09:18:43 +0530

    Added sign-out functionality to internal routes.

--- app/(main)/_components/AppHeader.jsx
+++ app/(main)/_components/AppHeader.jsx
@@ -1,6 +1,9 @@
 "use client"
 import { useAuthContext } from '@/app/provider'
+import { handleSignOut } from '@/app/utils/authHelper'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { SidebarTrigger } from '@/components/ui/sidebar'
+import { LogOut } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -10,13 +13,25 @@ function AppHeader() {
         <div className='p-3 flex justify-between items-center'>
             <SidebarTrigger />
             {user?.photoURL ? (
-                <Image
-                    src={user.photoURL}
-                    alt="User Profile Image"
-                    width={40}
-                    height={40}
-                    className="rounded-full"
-                />
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <div className="p-0.5 rounded-full hover:ring-2 hover:ring-neutral-700 transition duration-200">
+                            <Image
+                                src={user?.photoURL}
+                                alt="User"
+                                width={36}
+                                height={36}
+                                className="rounded-full object-cover cursor-pointer"
+                            />
+                        </div>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end" className="mt-2">
+                        <DropdownMenuItem onClick={handleSignOut} className="gap-2">
+                            <LogOut className="w-4 h-4" />
+                            Sign Out
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             ) : null}
         </div>
     )
